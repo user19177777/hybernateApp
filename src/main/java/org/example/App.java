e70f6aeee7a6e383d42d7868ad920cf3a94a54f5
Ivan Ivanov <79523701529@yandex.ru>
2022-09-04 16:54:05 +0300

    life circle of hibernate entity removed state

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -27,31 +27,11 @@ public class App
         Session session = sessionFactory.getCurrentSession();
         try {
             session.beginTransaction();
-            //transient state. hibernate не отслеживает сущности Person Passport
-            Person person = new Person("Den",35);
-            Passport  passport = new Passport(5555,person);
-            person.setPassport(passport);
-            //persistent(managed) state. Сущность переходит в отслеживаемое состояние,
-            //объекты-сущности переходят в Persistent context
-            //при вызове сеттеров будет генерироваться sql код(изменять данные в бд)
-            session.save(person);
-            session.save(passport);
 
-            System.out.println(person.getName());
-            //detached state. объекты-сущности перестают быть в Persistent context,
-            //не отслеживаются hibernate
-            //при вызове сеттеров не будет генерироваться sql код(не будет изменять данные в бд)
-            //имя в бд не изменится
-            session.detach(person);
-            person.setName("Ben");
-            System.out.println(person.getName());
-            //обратно помещает объект в Persistent context
-            //имя в бд изменится
-            session.merge(person);
-            person.setName("Ben");
-            System.out.println(person.getName());
+            Person person = session.get(Person.class,25);
 
-
+            //removed state
+            session.remove(person);
 
             session.getTransaction().commit();
         }finally {
